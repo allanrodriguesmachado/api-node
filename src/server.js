eab322907fed966ee1e42589bbe22d47124db107
ALLAN RODRIGUES MACHADO <38672505883@shibata.corp.br>
2024-08-31 12:40:46 -0300

    Recovering data in the body

--- src/server.js
+++ src/server.js
@@ -1,7 +1,20 @@
 import http from "node:http";
 
-const server = http.createServer((request, response) => {
-    return response.end("Hello World")
+const server = http.createServer(async (request, response) => {
+    const {method, url} = request;
+
+    if(method === "POST" && url === '/products') {
+        const buffers = [];
+
+        for await (const chunk of request) {
+            buffers.push(chunk);
+        }
+
+        console.log(Buffer.concat(buffers).toString());
+
+    }
+
+    return response.writeHead(200).end(`Hello World, ${url}`)
 });
 
 server.listen(8080)
