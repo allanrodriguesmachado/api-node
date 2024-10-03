e9efeafc9b1186250283b6a26cb0b93da0b3696a
Consultoria 01 <consultoria01@shibata.corp.br>
2024-10-03 09:26:35 -0300

    Clear Project

--- src/server.js
+++
@@ -1,4 +0,0 @@
-function person(first_name, last_name) {
-    console.log(first_name, last_name);
-}
-person('Allan', 'Rodrigues');
--- src/server.ts
+++
@@ -1,5 +0,0 @@
-function person(first_name: string, last_name: string) {
-    console.log(first_name, last_name);
-}
-
-person('Allan', 'Rodrigues');
