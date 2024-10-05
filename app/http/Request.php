bab3154c7250381ecdf3fa3af8893b65423b053b
Allan Rodrigues <allan.rodrigues14@hotmail.com>
2024-10-05 15:26:50 -0300

    Add method Request

---
+++ app/http/Request.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\http;
+
+class Request
+{
+    private string $httpMethod;
+    private string $uri;
+    private array $queryParams;
+    private array $postVars;
+    private array $headers;
+
+    public function __construct(
+    )
+    {
+        $this->queryParams = $_GET ?? [];
+        $this->postVars = $_POST ?? [];
+        $this->headers = getAllHeaders();
+        $this->httpMethod = $_SERVER['REQUEST_METHOD'] ?? '';
+        $this->uri = $_SERVER['REQUEST_URI'] ?? '';
+    }
+
+    public function getHttpMethod(): string {
+        return $this->httpMethod;
+    }
+
+    public function getUri(): string {
+        return $this->uri;
+    }
+
+    public function getQueryParams(): array {
+        return $this->queryParams;
+    }
+
+    public function getPostVars(): array {
+        return $this->postVars;
+    }
+
+    public function getHeaders(): array {
+        return $this->headers;
+    }
+}
--- public/index.php
+++ public/index.php
@@ -4,4 +4,7 @@ use App\Controller\Pages\Home;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
+$request = new \App\http\Request();
+dd($request);
+
 echo Home::getHome();
